ed52058e49bf1368acb3fa5d4d64ee4406e94f50
Oliver Malkin <oliver.malkin@gmail.com>
2022-11-01 01:29:11 +0000

    New broker

--- backend/broker.py
+++ backend/broker.py
@@ -4,47 +4,49 @@
 # Purpose:
 # Generates a new room code (4 character - alphaneumeric)
 # POST request /broker/makecode return code
-# GET request /broker/getip/{code} return IP
+# POST request /broker/removecode/{code} removes the code
+# GET reuqest /broker/getinfo/{code} returns info the broker has about the code
 
 import random, json, socket
-from flask import Flask, request
+from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
-games = {} # Stores all the game IP's and room codes
+games = {} # Stores all the game codes
 
 @app.route('/broker/makecode', methods=['POST'])
 def makecode():
     room_code = None
-    ip = request.remote_addr
 
-    for x in games:
-        if games[x] == ip:
-            return json.dumps({'code': None, 'status': 0})
-
-    while True:
-        room_code = ''.join([str(random.choice("123457890QWERTYUIOPASDFGHJKLZXCVBNM")) for i in range(4)])
+    while room_code not in games:
+        room_code = ''.join([str(random.choice("123457890QWERTYUIOPASDFGHJKLZXCVBNM")) for i in range(6)])
         if room_code not in games:
-            games[room_code] = ip
-        else:
-            break
+            games[room_code] = {
+                'time_created': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S+UTC"),
+                'status': 1
+                }
 
-    return json.dumps({'code': room_code, 'status': 1})     
+    return json.dumps({
+        'code': room_code,
+        'status': 1
+        }
+    )     
 
-@app.route('/broker/getip/<code>', methods=['GET'])
-def get_ip(code):
+@app.route('/broker/removecode/<code>', methods=['POST'])
+def remove_ip(code: str):
     try:
-        return json.dumps({'ip': games[code.upper()], 'status': 1})
-    except KeyError:
-        return json.dumps({'ip': None, 'status': 0})
-
-if __name__ == '__main__':
-    app.run(host=socket.gethostname())
-
-@app.route('/broker/removeip/<code>', methods=['POST'])
-def remove_ip(code):
-    try:
-        games.pop(code)
+        games.pop(code.upper())
         return {'status': 1}
     except:
         return {'status': 0}
+
+@app.route('/broker/getinfo/<code>', methods=['GET'])
+def getinfo(code: str):
+    try:
+        return json.dumps(games[code.upper()])
+    except KeyError:
+        return {'status': 0}
+
+if __name__ == '__main__':
+    app.run(host=socket.gethostname())
